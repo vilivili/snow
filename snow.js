ab55527f86a08c1e7eadd5a189dbb5d503df3c95
vilivili <vilivilivilivilivilivili@gmail.com>
2018-04-16 16:48:09 -0400

    Initial commit

--- snow.js
+++ snow.js
@@ -1,29 +1,26 @@
 var img;
 let bubbles = [];
 
+//prepare image
 function preload(){
   img = loadImage('imgs/snow man.jpg');
 }
 
 function setup() {
 createCanvas(windowWidth,windowHeight);
-
 }
-
+//click mouse, radius of snow balls is between 10 t0 30
 function mouseClicked(){
 let r = random(10,30);
-let b = new Bubble(mouseX,mouseY, r);
+let b = new Bubble(mouseX, mouseY, r);
+  // add a value to the end of the array, no matter the length of the array
 bubbles.push(b);
 
 }
 
-
-
 function draw() {
   imageMode(CENTER);
   image(img, windowWidth/2, windowHeight/2, windowHeight, windowHeight);
-
-
   for(let i = 0; i< bubbles.length; i++){
 bubbles[i].move();
 bubbles[i].show();
@@ -41,7 +38,7 @@ this.r = r;
 
 move(){
 
-  this.y = this.y+1;
+this.y = this.y+1;
 
 }
 
